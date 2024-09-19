e46dcdf4c026fa10f4ab27312bc5f61351d64ce9
Fahim Gharsa <58561079+fahimgha@users.noreply.github.com>
2024-09-19 16:59:25 +0200

    get and add books

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Books from "./components/books.vue";
+import AddBook from "./components/addBook.vue";
+import { ref, computed } from "vue";
+
+const books = ref([]);
+
+fetch("http://localhost:3000/getLibrary")
+  .then((response) => response.json())
+  .then((backendBooks) => {
+    books.value = backendBooks;
+    // console.log(backendBooks);
+  });
+</script>
+
+<template>
+  <div>
+    <h1>Track Books</h1>
+    <div className="app">
+      <Books :books="books" />
+      <AddBook />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  align-items: start;
+  justify-content: center;
+}
+</style>
